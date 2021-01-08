39f73c9c66e036cdad7f40e0833e3a2a2f700387
Raviraj2000 <ravirajshinde2000@gmail.com>
2021-01-08 15:13:09 +0530

    Minor Fixes

--- wasm/main.go
+++ wasm/main.go
@@ -625,7 +625,7 @@ func GetUptime() js.Func {
 				Start := time.Unix(StartTime, 0)
 				elapsed := time.Since(Start)
 				elapsed = elapsed.Round(time.Second)
-				SetDisplay("Time", "innerHTML", fmt.Sprintf("%s", durafmt.Parse(elapsed).LimitFirstN(2)))
+				SetDisplay("Time", "innerHTML", fmt.Sprintf("%s", durafmt.Parse(elapsed)))
 			}
 		}()
 		return nil
